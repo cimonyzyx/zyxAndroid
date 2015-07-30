8ee15c01a0a01f3bb8f10002dfe5b64c9315d5f5
cimonyzyx <724465481@qq.com>
2015-07-30 20:24:35 +0800

    add a int i

--- helloworld/src/com/zyx/helloworld/MainActivity.java
+++ helloworld/src/com/zyx/helloworld/MainActivity.java
@@ -6,18 +6,11 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    } 
     
 }
